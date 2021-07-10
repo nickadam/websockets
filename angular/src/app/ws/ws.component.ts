99ef660425d347e247c5adeb120a38000a725e32
Nick Vissari <networknerd@gmail.com>
2021-07-10 09:47:16 -0400

    Working example

---
+++ angular/src/app/ws/ws.component.ts
@@ -0,0 +1,104 @@
+import { Component, OnInit, OnDestroy } from '@angular/core'
+import { ActivatedRoute } from '@angular/router'
+import { interval, of } from 'rxjs'
+import { delay, tap } from 'rxjs/operators'
+import { webSocket, WebSocketSubject } from 'rxjs/webSocket'
+
+@Component({
+  selector: 'app-ws',
+  templateUrl: './ws.component.html',
+  styleUrls: ['./ws.component.sass']
+})
+export class WsComponent implements OnInit, OnDestroy {
+  connection$?: WebSocketSubject<any>
+  user = ''
+  data = ''
+  connect = false
+  error = ''
+  retryTime = 0
+  backoff = 1000 // 1 second
+  initialBackoff = 1000 // 1 second
+  maxBackoff = (1000 * 5 * 60) // 5 minutes
+
+  constructor(
+    private activatedRoute: ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    interval(1000).subscribe(_ => {
+      if(!this.connect) return
+
+      if(!this.connection$ || this.connection$.closed)
+        this.tryConnection()
+    })
+
+    //interval(1000).subscribe(n => {
+      //console.log(this.connection$?.closed)
+      // if(this.user && (!this.connection$ || this.connection$.closed)){
+      //   this.tryConnection()
+      // }
+
+      //this.connection$?.next('ho there ' + n)
+    //})
+    //this.connection$.error({code: 4000, reason: 'I think our app just broke!'})
+  }
+
+  ngOnDestroy(): void {
+    this.onDisconnect()
+  }
+
+
+  tryConnection(){
+    if(+new Date < this.retryTime) return
+
+    const host = window.location.host
+    const prot = window.location.protocol == 'https:' ? 'wss:' : 'ws:'
+    this.connection$ = webSocket(`${prot}//${host}/api/${this.user}`)
+    this.connection$.subscribe(
+      msg => {
+        this.resetRetryTime() // get some data reset backoff
+        this.data = msg.data.data
+        console.log('message received: ' + JSON.stringify(msg))
+      },
+      err => {
+        this.connection$?.unsubscribe()
+        this.error = JSON.stringify(err, ["message", "arguments", "type", "name"])
+
+        this.setRetryTime()
+
+        console.log('error', err)
+      },
+      () => console.log('complete')
+    )
+  }
+
+  onConnect(){
+    this.onDisconnect() // remove previous connections
+    this.connect = true
+    this.tryConnection()
+    console.log(this.user)
+  }
+
+  onDisconnect(){
+    this.connect = false
+    this.connection$?.complete()
+    this.connection$?.unsubscribe()
+  }
+
+  onUpdate(){
+    this.resetRetryTime() // sending data reset backoff
+    this.connection$?.next(this.data)
+    console.log(this.data)
+  }
+
+  resetRetryTime(){
+    this.backoff = this.initialBackoff
+    this.retryTime = 0
+  }
+
+  setRetryTime(){
+    this.backoff = this.backoff * 2
+    if(this.backoff > this.maxBackoff) this.backoff = this.maxBackoff
+    this.retryTime = +new Date + this.backoff
+  }
+}
---
+++ express/index.js
@@ -0,0 +1,128 @@
+const express = require('express')
+const WebSocket = require('ws')
+const { interval } = require('rxjs')
+const { Subject } = require('rxjs')
+const { nanoid } = require('nanoid')
+
+const authenticate = (request, next) => {
+  const user = request.url.replace('/', '')
+  console.log('authenticate ' + user)
+  if(!user) return next('nope')
+  next(null, {
+    user: user,
+    connection_id: nanoid()
+  })
+}
+
+const app = express()
+
+const wsSubjects = {}
+const wsPingIntervals = {}
+
+app.use(express.json())
+
+const wss = new WebSocket.Server({ noServer: true })
+wss.on('connection', (ws, request, user) => {
+
+  if(!wsSubjects[user.user]) wsSubjects[user.user] = new Subject()
+
+  const keepalive = interval(1000 * 30).subscribe(_ => { // 30 seconds
+    if (ws.connected === false){
+      console.log(`${(new Date).toISOString()} disconnecting ${user.user} connection ${user.connection_id} with ${subject.observers.length} observers`)
+      ws.terminate()
+      return
+    }
+    ws.connected = false
+    console.log(`${(new Date).toISOString()} sending ping for ${user.user} connection ${user.connection_id} with ${subject.observers.length} observers`)
+    ws.ping(_ => {})
+  })
+
+  const subject = wsSubjects[user.user]
+
+  const subscription = subject.subscribe(data => {
+    // message received from subject subscription, if it's from another connection send it
+    const stuff = {
+      data: data,
+      user: user
+    }
+    if(user.connection_id == data.sender.connection_id) return
+
+    console.log(`${(new Date).toISOString()} sending data`, stuff)
+    ws.send(JSON.stringify(stuff))
+  })
+
+  console.log(`${(new Date).toISOString()} received connection for ${user.user} connection ${user.connection_id} with ${subject.observers.length} observers`)
+
+  ws.on('message', msg => {
+    let message = {}
+    try {
+      message = JSON.parse(msg)
+    }catch (e) {
+      console.log('error parsing message')
+      return
+    }
+    //message received by client send to subject
+    console.log(`${(new Date).toISOString()} received message for ${user.user} connection ${user.connection_id} with ${subject.observers.length} observers`)
+    subject.next({
+      sender: user,
+      data: message
+    })
+  })
+
+  ws.on('pong', _ => {
+    console.log(`${(new Date).toISOString()} received pong for ${user.user} connection ${user.connection_id} with ${subject.observers.length} observers`)
+    ws.connected = true
+  })
+
+  ws.on('close', () => {
+    subscription.unsubscribe()
+    keepalive.unsubscribe()
+    console.log(`${(new Date).toISOString()} disconnected ${user.user} connection ${user.connection_id} with ${subject.observers.length} observers remaining`)
+  })
+})
+
+
+// this sends a ping to all clients at the same time
+// interval(1000 * 30).subscribe(_ => { // 30 seconds
+//   wss.clients.forEach(ws => {
+//     if (ws.connected === false){
+//       console.log('disconnecting')
+//       //ws.subscription.unsubscribe()
+//       ws.terminate()
+//       return
+//     }
+//     ws.connected = false
+//     ws.ping(_ => {})
+//   })
+// })
+
+// interval(1000).subscribe(_ => {
+//   console.log(wss.clients.size)
+// })
+
+// interval(1000).subscribe(n => {
+//   wss.clients.forEach((client) => {
+//     console
+//     //console.log(Object.keys(client))
+//     //if (client !== ws && client.readyState === WebSocket.OPEN){
+//     if (client.readyState === WebSocket.OPEN){
+//       console.log('sending data')
+//       client.send(JSON.stringify('hi there ' + n))
+//     }
+//   })
+// })
+
+const server = app.listen(3000)
+server.on('upgrade', (request, socket, head) => {
+  authenticate(request, (err, user) => {
+    if (err || !user) {
+      socket.write('HTTP/1.1 401 Unauthorized\r\n\r\n')
+      socket.destroy()
+      return
+    }
+
+    wss.handleUpgrade(request, socket, head, ws => {
+      wss.emit('connection', ws, request, user)
+    })
+  })
+})
